bb7be0afd84644733f1f2922e8421cb1d42d0425
cbj2741@gmail.com <cbj2741@naver.com>
2022-06-09 21:59:38 +0900

    java lec 12

---
+++ JavaStudy/src/lec11/Bicycle.java
@@ -0,0 +1,22 @@
+package lec11;
+
+public class Bicycle {
+	
+	public String color;
+	public int price;
+	
+	public Bicycle() {
+		System.out.println("Bicycle constructor");
+	}
+	public Bicycle(String c,int p) {
+		System.out.println("Bicycle constructor");
+		this.color = c;
+		this.price = p;
+	}
+	
+	public void info() {
+		System.out.println(" -- info() --");
+		System.out.println("color :" +color);
+		System.out.println("price :" +price);
+	}
+}
---
+++ JavaStudy/src/lec11/MainClass.java
@@ -0,0 +1,25 @@
+package lec11;
+
+public class MainClass {
+	public static void main(String[] args) {
+		
+		Grandeur myCar1 = new Grandeur();
+		myCar1.color = "red";
+		myCar1.gear = "auto";
+		myCar1.price = 100;
+		
+		myCar1.run();
+		myCar1.stop();
+		myCar1.info();
+		
+		Bicycle myBicycle = new Bicycle();
+		myBicycle.color = "red";
+		myBicycle.price = 100;
+		myBicycle.info();
+		
+		Bicycle myBicycle2 = new Bicycle("blue",100);
+		myBicycle2.info();
+		myBicycle2.color = "yellow";
+		myBicycle2.info();
+	}
+}
