c403c60e142994680e64db863d83aaff4ac3aa64
cbj2741@gmail.com <cbj2741@naver.com>
2022-06-12 22:09:19 +0900

    lec18 상속

---
+++ JavaStudy/src/lec18_상속/ChildClass.java
@@ -0,0 +1,11 @@
+package lec18_»ó¼Ó;
+
+public class ChildClass extends ParentClass {
+	public ChildClass() {
+		System.out.println("Child Constructor");
+		// TODO Auto-generated constructor stub
+	}
+	public void childFunc() {
+		System.out.println("childFunc()");
+	}
+}
